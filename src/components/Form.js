b2aef410160e62eaed87047f6934b8c33c5f87ab
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-01-15 13:28:53 +0300

    fix

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Form = () => {
+  return (
+    <View>
+      <TextInput></TextInput>
+    </View>
+  )
+}
+
+export default Form;
