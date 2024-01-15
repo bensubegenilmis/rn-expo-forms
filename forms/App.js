fbcb1bfaa84e6d368f8ab69084c59cb438258b05
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-01-15 17:03:30 +0300

    newform

--- forms/App.js
+++ forms/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Form from 'forms/src/components/Form.js';
+import Form from './src/components/Form.js';
 
 
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Form />
     </View>
   );
